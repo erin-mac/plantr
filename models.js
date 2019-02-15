a74459f7999aaafce5257f836eb0672666165e7f
Grant Horner <h.grant.horner@gmail.com>
2019-02-14 21:24:25 -0500

    fuqqed that shit up hardcore bb

--- models.js
+++ models.js
@@ -2,27 +2,30 @@ const Sequelize = require('sequelize')
 const db = new Sequelize('postgres://localhost:5432/plantr')
 
 const Gardener = db.define('gardener', {
-    name: Sequelize.STRING,
-    age: Sequelize.INTEGER
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
 })
 
 const Plot = db.define('plot', {
-    size: Sequelize.INTEGER,
-    shaded: Sequelize.BOOLEAN
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
 })
 
 const Vegetable = db.define('vegetable', {
-    name: Sequelize.STRING,
-    color: Sequelize.STRING,
-    planted_on: Sequelize.DATE
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
 })
 
 Plot.belongsTo(Gardener)
 Gardener.hasOne(Plot)
-Plot.belongsToMany(Vegetable, { through: "garden" })
-Vegetable.belongsToMany(Plot, { through: "garden" })
-Gardener.belongsTo(Vegetable, { as: 'fav_veg_id' })
+Plot.belongsToMany(Vegetable, { through: 'garden' })
+Vegetable.belongsToMany(Plot, { through: 'garden' })
+Gardener.belongsTo(Vegetable, { as: 'favVeg' })
 
 module.exports = {
-    db
+  db,
+  Plot,
+  Gardener,
+  Vegetable
 }
--- seed.js
+++ seed.js
@@ -1,5 +1,82 @@
-const { db } = require('./models');
+const { db, Plot, Vegetable, Gardener } = require('./models')
 
-db.authenticate()
+function seedThisShit() {
+  db.authenticate()
     .then(() => db.sync({ force: true }))
-    .then(() => { db.close() })
+    .then(async () => {
+      const [carrot, potatoes, kale] = await Promise.all([
+        Vegetable.create({
+          name: 'carrot',
+          color: 'purple',
+          planted_on: new Date(Date.UTC(2016, 0, 1))
+        }),
+        Vegetable.create({
+          name: 'potatoes',
+          color: 'orange',
+          planted_on: new Date(Date.UTC(2018, 11, 1))
+        }),
+        Vegetable.create({
+          name: 'kale',
+          color: 'blood',
+          planted_on: new Date(Date.UTC(0, 11, 25))
+        })
+      ])
+      const [jerry, elaine] = await Promise.all([
+        Gardener.create({
+          name: 'jerry',
+          age: 5,
+          favVegId: kale.id
+        }),
+        Gardener.create({
+          name: 'elaine',
+          age: 95,
+          favVegId: kale.id
+        })
+      ])
+      const [jerrysPlot, elainesPlot] = await Promise.all([
+        Plot.create(
+          {
+            size: 1500,
+            shaded: true,
+            gardenerId: jerry.id,
+            vegetables: [
+              {
+                name: kale.name
+              },
+              {
+                name: potatoes.name
+              }
+            ]
+          },
+          {
+            include: Vegetable
+          }
+        ),
+        Plot.create(
+          {
+            size: 9000,
+            shaded: true,
+            gardenerId: elaine.id,
+            vegetables: [
+              {
+                name: kale.name
+              },
+              {
+                name: carrot.name
+              }
+            ]
+          },
+          {
+            include: Vegetable
+          }
+        )
+      ])
+    })
+    .then(() => {
+      db.close()
+    })
+}
+
+seedThisShit()
+
+module.exports = seedThisShit
